4d38d9dc6c3a34b30e0faea6fd19cfea9616bd2e
aryaelmahdi <106580305+aryaelmahdi@users.noreply.github.com>
2023-05-23 22:13:48 +0700

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,24 +1,69 @@
-let id = document.getElementById("number");
-let id2 = document.getElementById("number2");
-let size = $(window).width();
+function validate() {
+    let email = document.getElementById("email").value;
+    let produk = document.getElementById("produk").value;
+    let jam = document.getElementById("jam").value;
+    let durasi = document.getElementById("durasi").value;
 
-console.log(size);
 
-function hideIt() {
-    var value = parseInt(document.getElementById('number').value, 3);
-    value = isNaN(value) ? 1 : value;
-    value++;
-    document.getElementById('number').value = value;
-    console.log(value)
-    if (value > 2) {
-        document.getElementById("number2").style.display = "none";
-        document.getElementById("number").style.display = "block";
-        value = 1;
-        return value;
-    } else if (value < 2) {
-        document.getElementById("number2").style.display = "block";
-        document.getElementById("number2").style.display = "none";
-        value = 2;
-        return value;
+    let dur = "false";
+    let pro = "false";
+    let mail = "false";
+    let time = "false";
+    if (!email.includes("@")) {
+        alert("Please input your email");
+        mail = "false";
+    } else {
+        mail = "true";
+    }
+
+    if (produk == "select your room") {
+        alert("Please select your room");
+        pro = "false";
+    } else {
+        pro = "true";
+    }
+
+    if (jam == "select your time") {
+        alert("please select your time");
+        time = "false";
+    } else {
+        time = "true";
+    }
+
+    if (durasi == "select your duration") {
+        alert("Please select your duration");
+        dur = "false";
+    } else {
+        dur = "true";
+    }
+
+    console.log(mail);
+    console.log(pro);
+    console.log(dur);
+    console.log(time);
+
+    if (mail == "true" && pro == "true" && dur == "true" && time == "true") {
+        dur = "false";
+        pro = "false";
+        mail = "false";
+        time = "false";
+        console.log(mail);
+        console.log(pro);
+        console.log(dur);
+        console.log(time);
+        location.assign("sukses.html");
     }
 }
+
+//
+var roomImages = ["", "images/11.jpg", "images/12.jpg", "images/13.jpg", "images/14.jpg", "images/15.jpg", "images/16.jpg", "images/17.jpg", "images/18.jpg"];
+var currentImageIndex = 0;
+var roomSelect = document.getElementById("produk");
+
+roomSelect.addEventListener("change", function() {
+    var roomIndex = roomSelect.selectedIndex;
+    if (roomIndex >= 0 && roomIndex < roomImages.length) {
+        currentImageIndex = roomIndex;
+        document.getElementById("room-image").src = roomImages[currentImageIndex];
+    }
+});
